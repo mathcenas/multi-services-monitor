f959aa139abafb843ac4e3af45eaab5c3a8b0b36
Mathias Cenas <37383408+mathcenas@users.noreply.github.com>
2025-12-28 22:12:48 -0300

    Updated monitor-agent.sh

--- monitor-agent.sh
+++ monitor-agent.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-API_URL="https://stats.cenas-support.com/api"
+API_URL="${MONITOR_API_URL:-https://stats.cenas-support.com}/api"
 SERVER_NAME=$(hostname)
 SERVER_ID="${SERVER_ID:-1}"
 CHECK_INTERVAL="${CHECK_INTERVAL:-60}"
